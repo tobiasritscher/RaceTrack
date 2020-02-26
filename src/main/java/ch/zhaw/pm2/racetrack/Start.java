66045b55562a37f75670eb71db25fe79ef3a996f
Tobias Ritscher <ritsctob@students.zhaw.ch>
2020-02-26 13:17:32 +0100

    added TextIO

--- src/main/java/ch/zhaw/pm2/racetrack/Start.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Start.java
@@ -1,8 +1,22 @@
 package ch.zhaw.pm2.racetrack;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
 public class Start {
 
     public Start() {
 
     }
+
+    public static void main(String[] args) {
+
+    }
+
+    private void setUpGame() throws FileNotFoundException {
+        //init new UserInterface
+        new File("tracks/", "challange.txt");
+    }
+
 }
