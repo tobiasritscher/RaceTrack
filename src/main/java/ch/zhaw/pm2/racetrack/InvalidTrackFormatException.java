4e83b1cfdef3c6b4aa9150641d5ba0a52f43758c
Oliver <oliver.corrodi@students.zhaw.ch>
2020-02-26 11:12:59 +0100

    Added InvalidTrackFormatException class.
    Error type handling still to be implemented using enum.

---
+++ src/main/java/ch/zhaw/pm2/racetrack/InvalidTrackFormatException.java
@@ -0,0 +1,32 @@
+package ch.zhaw.pm2.racetrack;
+
+import java.io.File;
+
+/**
+ * Checked exception which is thrown then the track format doesn't match the requirements
+ * of the game.
+ *
+ * Error Types:
+ *   1: not all track lines have the same length</li>
+ *   2: the file contains no track lines (grid height is 0)</li>
+ *   3: the file contains more than {@link Config#MAX_CARS} cars</li>
+ *
+ * @author corrooli
+ * @version 200226
+ */
+
+public class InvalidTrackFormatException extends Exception {
+    private File trackFile;
+    private int errorType;
+
+    public InvalidTrackFormatException(File trackFile){ this.trackFile = trackFile; }
+
+    public File getTrackFile() { return trackFile; }
+
+    public String toString(){
+        return("The following track file doesn't match the requirements:\n" + trackFile.toString() +
+                "Error Type <to be implemented>");
+    }
+}
+
+
