ac6bf68b7e73e3607cec8915e0376f8981fc8601
erman <zankoerm@students.zhaw.ch>
2020-02-27 13:29:38 +0100

    started the building stones for the beginning of the game

--- src/main/java/ch/zhaw/pm2/racetrack/Start.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Start.java
@@ -1,18 +1,23 @@
 package ch.zhaw.pm2.racetrack;
 
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+import org.beryx.textio.TextTerminal;
+import org.beryx.textio.swing.SwingTextTerminal;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import org.beryx.textio.*;
 
 
 public class Start {
-
+    private static TextIO textIO = TextIoFactory.getTextIO();
+    private static TextTerminal<SwingTextTerminal> textTerminal = (SwingTextTerminal) textIO.getTextTerminal();
     public Start() {
 
     }
 
     public static void main(String[] args) {
-
+    amountOfPlayers();
     }
 
     private void setUpGame() throws FileNotFoundException {
@@ -20,4 +25,8 @@ public class Start {
         new File("tracks/", "challange.txt");
     }
 
+    public static void amountOfPlayers(){
+        Integer players = textIO.newIntInputReader().withMinVal(2).withMaxVal(9).read("How many players will be playing?");
+
+    }
 }
