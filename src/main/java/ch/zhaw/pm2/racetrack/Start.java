023d29b8f3a1ed10146aa3d1ae22710147ef5d04
Tobias Ritscher <ritsctob@students.zhaw.ch>
2020-03-03 18:39:50 +0100

    you can choose your map in the beinning

--- src/main/java/ch/zhaw/pm2/racetrack/Start.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Start.java
@@ -8,6 +8,8 @@ import org.beryx.textio.swing.SwingTextTerminal;
 import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class Start {
@@ -31,8 +33,13 @@ public class Start {
     }
 
     private static void setUpGame() throws FileNotFoundException {
-        File files = new File("tracks/");
-        textIO.newIntInputReader().withMinVal(1).withMaxVal(4).read(files.toString());
+        File files = new File("tracks");
+        String[] tracks = Objects.requireNonNull(files.list());
+        for (int i = 0; i < tracks.length; ++i){
+            textTerminal.print(i + 1 + ": " + tracks[i] + "\n");
+        }
+        textIO.newIntInputReader().withMinVal(1).withMaxVal(tracks.length)
+                .read("Choose your map [1-" + tracks.length + "]:");
     }
 
     public static void amountOfPlayers() {
@@ -59,8 +66,8 @@ public class Start {
      * lets the players decide on their strategies for the game
      */
     public static void strategies() {
-        for (int i = 0; i < carArray.length; ++i) {
-            textTerminal.print(carArray[i].getName() + " what do you want your strategy to be?");
+        for (Car value : carArray) {
+            textTerminal.print(value.getName() + " what do you want your strategy to be?");
             textTerminal.print("1: DO_NOT_MOVE");
             textTerminal.print("2:USER");
             Integer choice = textIO.newIntInputReader().read("Please choose");
@@ -68,7 +75,7 @@ public class Start {
             do {
                 switch (choice) {
                     case 1:
-                        carArray[i].byMachine();
+                        value.byMachine();
                         break;
                     case 2:
                         // TODO implement user strategy
