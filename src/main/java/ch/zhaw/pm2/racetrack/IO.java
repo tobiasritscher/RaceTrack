97fe6e6221ec37ef16cab0a5b7314331ce731f75
Tobias Ritscher <ritsctob@students.zhaw.ch>
2020-03-04 10:36:06 +0100

    Input and output is now over the IO class

--- src/main/java/ch/zhaw/pm2/racetrack/IO.java
+++ src/main/java/ch/zhaw/pm2/racetrack/IO.java
@@ -1,9 +1,28 @@
 package ch.zhaw.pm2.racetrack;
 import org.beryx.textio.*;
+import org.beryx.textio.swing.SwingTextTerminal;
 
 public class IO {
+    private static TextIO textIO = TextIoFactory.getTextIO();
+    private static TextTerminal<SwingTextTerminal> textTerminal = (SwingTextTerminal) textIO.getTextTerminal();
 
     public IO() {
+
     }
 
+    public void print(String output){
+        textTerminal.print(output);
+    }
+
+    public int intInputReader(int min, int max, String output) {
+        return textIO.newIntInputReader().withMinVal(min).withMaxVal(max).read(output);
+    }
+
+    public char charInputReader(String output) {
+        return textIO.newCharInputReader().read(output);
+    }
+
+    public boolean booleanInputReader(String output) {
+        return textIO.newBooleanInputReader().read(output);
+    }
 }
--- src/main/java/ch/zhaw/pm2/racetrack/Start.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Start.java
@@ -1,20 +1,14 @@
 package ch.zhaw.pm2.racetrack;
 
-import org.beryx.textio.TextIO;
-import org.beryx.textio.TextIoFactory;
-import org.beryx.textio.TextTerminal;
-import org.beryx.textio.swing.SwingTextTerminal;
-
 import java.awt.*;
 import java.io.File;
 import java.util.Objects;
 
 
 public class Start {
-    private static TextIO textIO = TextIoFactory.getTextIO();
-    private static TextTerminal<SwingTextTerminal> textTerminal = (SwingTextTerminal) textIO.getTextTerminal();
     static Car car = new Car();
     private static Car[] carArray;
+    static IO io = new IO();
 
     public Start() {
 
@@ -30,16 +24,16 @@ public class Start {
     private static void setUpGame() {
         File files = new File("tracks");
         String[] tracks = Objects.requireNonNull(files.list());
+
+        io.print("Welcome to Racetrack!\nSelect Track file:\n");
         for (int i = 0; i < tracks.length; ++i) {
-            textTerminal.print(i + 1 + ": " + tracks[i] + "\n");
+            io.print("  " + (i + 1) + ": " + tracks[i] + "\n");
         }
-        int track = textIO.newIntInputReader().withMinVal(1).withMaxVal(tracks.length)
-                .read("Choose your map [1-" + tracks.length + "]:");
+        int track = io.intInputReader(1, tracks.length, "Choose your map [1-" + tracks.length + "]:");
     }
 
     public static void amountOfPlayers() {
-        int players = textIO.newIntInputReader().withMinVal(2).withMaxVal(9)
-                .read("How many players will be playing [2-9]: ");
+        int players = io.intInputReader(2, 9, "How many players will be playing [2-9]: ");
         carArray = new Car[players];
         for (int i = 0; i < players; ++i) {
             carArray[i] = new Car();
@@ -53,7 +47,7 @@ public class Start {
      */
     public static void nameOfPlayers(Point position) {
         for (int i = 0; i < carArray.length; ++i) {
-            char name = textIO.newCharInputReader().read("Please give your car a character: ");
+            char name = io.charInputReader("Please give your car a character: ");
             carArray[i] = new Car(position, name);
         }
     }
@@ -63,10 +57,10 @@ public class Start {
      */
     public static void strategies() {
         for (Car value : carArray) {
-            textTerminal.print(value.getName() + " what do you want your strategy to be?");
-            textTerminal.print("1: DO_NOT_MOVE");
-            textTerminal.print("2: USER");
-            int choice = textIO.newIntInputReader().read("Please choose [1,2]: ");
+            io.print(value.getName() + " what do you want your strategy to be?");
+            io.print("1: DO_NOT_MOVE");
+            io.print("2: USER");
+            int choice = io.intInputReader(1,2,"Please choose [1,2]: ");
             boolean invalidChoice = false;
             do {
                 switch (choice) {
@@ -77,7 +71,7 @@ public class Start {
                         // TODO implement user strategy
                         break;
                     default:
-                        textTerminal.print("This isnt a valid choice, please choose again");
+                        io.print("This isnt a valid choice, please choose again");
                         invalidChoice = true;
                 }
             } while (invalidChoice);
