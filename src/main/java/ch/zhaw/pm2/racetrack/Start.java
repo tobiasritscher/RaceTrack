f34166f5298d1cb7b5ca1d9f9f883ddbbb5423e0
Tobias Ritscher <ritsctob@students.zhaw.ch>
2020-03-18 10:22:43 +0100

    added a linebreak

--- src/main/java/ch/zhaw/pm2/racetrack/Start.java
+++ src/main/java/ch/zhaw/pm2/racetrack/Start.java
@@ -81,7 +81,7 @@ public class Start {
             io.refresh(track);
         } while (Game.NO_WINNER == game.getWinner());
         io.print(game.getCarId(game.getCurrentCarIndex()) + " is the winner");
-        io.print("Hit 'Enter' to quit the game");
+        io.print("\nHit 'Enter' to quit the game");
         io.promptEnter();
         io.closeTerminal();
     }
