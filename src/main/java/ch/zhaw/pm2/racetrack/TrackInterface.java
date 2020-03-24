010f35c7024315962769536646648828272457ea
Nikita Smailov <smailnik@students.zhaw.ch>
2020-03-24 02:06:43 +0100

    change functions order in TrackInterface

--- src/main/java/ch/zhaw/pm2/racetrack/TrackInterface.java
+++ src/main/java/ch/zhaw/pm2/racetrack/TrackInterface.java
@@ -5,33 +5,33 @@ import java.util.List;
 public interface TrackInterface {
     Config.SpaceType[][] getGrid();
 
-    int getCarCount();
-
     char getCarId(int index) throws IllegalArgumentException;
 
     PositionVector getCarPosition(int index) throws IllegalArgumentException;
 
+    PositionVector getCarNextPosition(int carIndex) throws IllegalArgumentException;
+
     PositionVector getCarVelocity(int index) throws IllegalArgumentException;
 
-    Config.SpaceType getSpaceType(PositionVector position) throws IllegalArgumentException;
-
-    List<Car> getCars();
-
-    Car getCar(int carIndex) throws IllegalArgumentException;
-
-    boolean isSomeOtherCarHere(int currentCarIndex, PositionVector position) throws IllegalArgumentException;
-
     void accelerateCar(int carIndex, PositionVector.Direction acceleration) throws IllegalArgumentException;
-
-    PositionVector getCarNextPosition(int carIndex) throws IllegalArgumentException;
 
     void crashCar(int carIndex, PositionVector crashLocation) throws IllegalArgumentException;
 
     void moveCar(int carIndex) throws IllegalArgumentException;
 
-    boolean isCarCrashed(int carIndex) throws IllegalArgumentException;
+    int getCarCount();
 
     int getNumberActiveCarsRemaining();
+
+    List<Car> getCars();
+
+    Car getCar(int carIndex) throws IllegalArgumentException;
+
+    Config.SpaceType getSpaceType(PositionVector position) throws IllegalArgumentException;
+
+    boolean isSomeOtherCarHere(int currentCarIndex, PositionVector position) throws IllegalArgumentException;
+
+    boolean isCarCrashed(int carIndex) throws IllegalArgumentException;
 
     boolean isOnFinishLine(PositionVector position) throws IllegalArgumentException;
 
